12eb4a460782a3471baf0ba0931ff15ddb6cf957
Yuta Nagamiya <y.nagamiya@gmail.com>
2011-10-09 20:04:42 +0900

    新規作成

---
+++ server.js
@@ -0,0 +1,23 @@
+var io = require('socket.io').listen(8080);
+
+io.sockets.on('connection', function(socket) {
+  console.log('onconnection:', socket);
+
+  // クライアントからのイベント'all'を受信する
+  socket.on('all', function(data) {
+    // イベント名'msg'でオブジェクトを
+    // 自分を含む全クライアントにブロードキャストする
+    io.sockets.emit('msg', data);
+  });
+
+  // クライアントからのイベント'others'を受信する
+  socket.on('others', function(data) {
+    // イベント名'msg'でオブジェクトを
+    // 自分以外の全クライアントにブロードキャストする
+    socket.broadcast.emit('msg', data);
+  });
+
+  socket.on('disconnect', function() {
+    console.log('disconn');
+  });
+});
